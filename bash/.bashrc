fdae671699267e029fe9b54e49d79f8dc2105084
Tume Marenk <tuomas.marenk@gmail.com>
2023-02-28 16:41:26 +0200

    Reorganized structure, better install script

--- bash/.bashrc
+++ bash/.bashrc
@@ -22,7 +22,9 @@ if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
     debian_chroot=$(cat /etc/debian_chroot)
 fi
 
-for DOTFILE in `find ~/.dots/bash`
+# Load modules from dotfiles
+
+for DOTFILE in `find ~/.dots/bash/modules`
 do
 	[ -f "$DOTFILE" ] && . "$DOTFILE"
 done
--- install.sh
+++ install.sh
@@ -5,5 +5,19 @@
 # Place for files
 dir=$1
 
-# Link files
-stow --target=$dir home/
+# Link config files
+for file in $(find .config -type f)
+do
+    # create directories for config files if they do not exists yet
+	subdir="${file%/*}/"
+	if [[ ! -d "$dir/$subdir" ]]
+	then
+		mkdir -p $dir/$subdir
+	fi
+	ln $file $dir/$file
+done
+
+# Link bash files
+
+ln bash/.bashrc $dir/.bashrc
+ln bash/.bash_profile $dir/.bash_profile
